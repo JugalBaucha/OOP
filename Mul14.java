b32428beae8726cf70d859adf1ee69718ab09b1f
JugalBaucha <108106662+JugalBaucha@users.noreply.github.com>
2022-09-26 10:49:58 +0545

    Create Mul14.java

---
+++ Mul14.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Mul14{
+    public static void main(String[]args)
+    {
+        int i,j;
+        for(i=1;i<=10;i++)
+        {
+            for(j=1;j<=10;j++)
+            {
+                int result=i*j;
+                System.out.println(i+"*"+j+"="+result);
+            }
+            System.out.println("\n");
+        }
+    }
+}
