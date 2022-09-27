53a38e1b3beceef97780f81f7873df9974ff442b
JugalBaucha <108106662+JugalBaucha@users.noreply.github.com>
2022-09-27 08:24:12 +0545

    Add files via upload

---
+++ Square18.java
@@ -0,0 +1,10 @@
+class Square18 {
+    public static void main(String[] args) {
+        int i;
+        System.out.println("NUMBERS");
+        for (i = 1; i < 10; i++) {
+
+            System.out.println(i + "=" + Math.pow(i, 2));
+        }
+    }
+}
